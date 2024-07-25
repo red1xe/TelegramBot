14be8f73e6e0e121f3b5885a214d5fa4939532b5
Selim Berat <selimberatuysal@gmail.com>
2024-07-25 12:46:26 +0300

    Photo bot upgrade

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String BOT_TOKEN = System.getenv("TOKEN");
+        try {
+            TelegramBotsLongPollingApplication botsApplication = new TelegramBotsLongPollingApplication();
+            botsApplication.registerBot(BOT_TOKEN, new MySimpleBot());
+            Thread.currentThread().join();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
